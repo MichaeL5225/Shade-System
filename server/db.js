3bbcd63421151c62809e1956e8f6f3b7cc1b409a
GuyC21 <e4guycohen@outlook.com>
2025-09-05 16:50:02 +0300

    v1

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2/promise'); // חשוב להשתמש ב-promise
+const mysql = require("mysql2/promise"); // חשוב להשתמש ב-promise
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '@oduek1309$',
-  database: 'shade_system_db',
+  host: "localhost",
+  user: "root",
+  password: "0021",
+  database: "shade_system_db",
   waitForConnections: true,
   connectionLimit: 10, // מספר חיבורים בו זמנית
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = pool;
