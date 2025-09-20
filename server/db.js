4d01326a4584580f9b3e5494619a89f01825f127
Ofir Duek <GitHub - ofirduek1@gmail.com>
2025-09-20 17:01:44 +0300

    last update

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise"); // חשוב להשתמש ב-promise
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "0021",
+  password: "@oduek1309$",
   database: "shade_system_db",
   waitForConnections: true,
   connectionLimit: 10, // מספר חיבורים בו זמנית
